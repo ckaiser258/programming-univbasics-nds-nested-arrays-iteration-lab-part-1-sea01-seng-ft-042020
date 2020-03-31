ac4aadd4d73b0c35b833b175b90045b76c7f2310
Colton Kaiser <kaiserc2@seattleu.edu>
2020-03-31 02:38:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,8 +8,8 @@ def find_even_values(src)
     
     inner_count = 0
     while inner_count < src[count].length do
-      if src[count] % 2 == 0
-        p src[count]
+      if src[count][inner_count] % 2 == 0
+        p src[count][inner_count]
       end
       inner_count += 1
     end
